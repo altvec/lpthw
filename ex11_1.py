6bfc9ae3554bb2e6bf42e41be0488005331fffc2
altvec <sergey@bluehat.me>
2014-08-23 21:06:36 +0600

    Exercise 11

---
+++ ex11_1.py
@@ -0,0 +1,7 @@
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weight? ")
+hair = raw_input("What colour is your hair? ")
+print "So, you're %r old, %s tall and %s heavy." % (
+    age, height, weight),
+print "And your hair is %s" % hair
