9ef3fe6c46889c1629fb36c097e4754af36f738c
altvec <sergey@bluehat.me>
2015-01-17 22:51:33 +0500

    More

--- ex36.py
+++ ex36.py
@@ -2,21 +2,57 @@ from sys import exit
 
 print "Adventures of the Jedi"
 
+WITH_MASTER = False
+
 def mountain():
-    print "There is a lonely mountain right ahead"
+    global WITH_MASTER
+    print "\nThere is a lonely mountain right ahead of you."
+    print "You can see a strange spiral sign on the rock."
+    print "1. Touch the sign"
+    print "2. Yell at sign"
+    print "3. Go back"
+    choice = raw_input("> ")
+    if choice == "touch" and WITH_MASTER == False:
+        print "Nothing happened."
+        mountain()
+    elif choice == "touch" and WITH_MASTER == True:
+        print "Mount opens and you see grand master Yoda."
+        dead("Congratulations, you win!")
+    elif choice == "yell":
+        dead("A giant rock fall of the cliff and smashed you.")
+    elif choice == "back":
+        start()
+    else:
+        print "Probably doing {} is not great idea.".format(choice)
+        mountain()
 
 def emptyness():
-    print "There is nothing interesting here."
+    dead("Giant desert worm eats you.")
 
 def space_ship():
-    print "This is your space ship"
+    global WITH_MASTER
+    print "\nThis is your space ship and you master is awaiting you."
+    print "1. Ask a master to go with you."
+    print "2. Kick master's butt."
+    print "3. Go back"
+    choice = raw_input("> ")
+    if choice == "ask":
+        print "Master go with you."
+        WITH_MASTER = True
+        space_ship()
+    elif choice == "kick":
+        dead("Master cuts your head off.")
+    elif choice == "back":
+        start()
+    else:
+        dead("You stumble around and fell on your lightsaber.")
 
 def dead(cause):
     print cause
     exit(0)
 
 def start():
-    print "You have found yourself in the hot desert on unknown planet."
+    print "\nYou have found yourself in the hot desert on unknown planet."
     print "You look around and see lonely mountain on North,"
     print "barely visible space ship on East,"
     print "clouds of dust on South and West."
@@ -29,7 +65,6 @@ def start():
     elif "East" in choice:
         space_ship()
     else:
-        dead("Attempting to {0} you're stuck in the sand, and after a few \
-hours of unsuccessful attempts to escape - died.".format(choice))
+        dead("Attempting to {} you're stuck in the sand, and after a few hours of unsuccessful attempts to escape - died.".format(choice))
 
 start()
