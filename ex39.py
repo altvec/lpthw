c8e41daa14a0b6b90fb7e4f1bf4dfd7029388ec7
altvec <sergey@bluehat.me>
2015-02-03 00:04:50 +0500

    Fixed typo

--- ex39.py
+++ ex39.py
@@ -46,7 +46,7 @@ for abbrev, city in cities.items():
 # now do both at the same time
 print '-' * 10
 for state, abbrev in states.items():
-    print "%s satet is abbreviated %s and has city %s" % (state, abbrev,
+    print "%s state is abbreviated %s and has city %s" % (state, abbrev,
                                                           cities[abbrev])
 
 print '-' * 10
